f6c2efa7ff54c89e7f39226d8b344972b02cfee2
Benoit Patra <benoit.patra@gmail.com>
2020-11-29 15:54:42 +0100

    Cosmetics from IDE

--- src/main/scala/TreeStructureBuilder.scala
+++ src/main/scala/TreeStructureBuilder.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 object TreeStructureBuilder{
 
   def buildTreeFromPathList(filePaths: List[List[String]]): Node = {
@@ -12,22 +14,21 @@ object TreeStructureBuilder{
     case head::tail => Node.apply(head, List(buildSingleBranchedTree(tail)))
   }
 
+  @tailrec
   def mergePathIntoTree(tree: Node, filePath: List[String]) : Unit = (tree, filePath) match {
     case (Node(value, _), head :: _) if value != head => throw new Exception("Recursive invariant condition not satisfied")
     case (Node(_, _), _ :: Nil) => // tail list is empty there is nothing more to do
-    case (node@Node(_ , children), _ :: tail) => { // by loop invariant node.value hand matched list head are equal
+    case (node@Node(_ , children), _ :: tail) => // by loop invariant node.value hand matched list head are equal
       // first, let's see if there is already a children that could be match for our branch next value
       val targetChild = children.find(child => child match {
         case Node(value, _) => value == tail.head //branch next value: tail.value exists because of previous matches
       })
       targetChild match {
         case Some(childValue) => mergePathIntoTree(childValue, tail)
-        case None => {
+        case None =>
           val newChild = Node(tail.head, List())
           node.addChild(newChild)
           mergePathIntoTree(newChild, tail)
-        }
       }
-    }
   }
 }
